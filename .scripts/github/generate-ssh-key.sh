5d1665ea739409dcfe458c20c90f489cb02e5780
Chris Hayward <chayward1@athabasca.edu>
2020-02-25 14:43:28 -0700

    Remove email from scripts

--- .scripts/github/generate-ssh-key.sh
+++ .scripts/github/generate-ssh-key.sh
@@ -1,3 +1,3 @@
-ssh-keygen -t rsa -b 4096 -N '' -C "chayward1@athabasca.edu" -f ~/.ssh/id_rsa
-ssh-keygen -t rsa -b 4096 -N '' -C "chayward1@athabasca.edu" -f ~/.ssh/github_rsa
-ssh-keygen -t rsa -b 4096 -N '' -C "chayward1@athabasca.edu" -f ~/.ssh/mozilla_rsa
+ssh-keygen -t rsa -b 4096 -N '' -C "YOUR-EMAIL-HERE" -f ~/.ssh/id_rsa
+ssh-keygen -t rsa -b 4096 -N '' -C "YOUR-EMAIL-HERE" -f ~/.ssh/github_rsa
+ssh-keygen -t rsa -b 4096 -N '' -C "YOUR-EMAIL-HERE" -f ~/.ssh/mozilla_rsa
